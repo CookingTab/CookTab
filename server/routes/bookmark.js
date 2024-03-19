762f50c3f1074db147451c4ce8251fb937efc83a
NatasyaPutriS <natasyaputri.s@gmail.com>
2024-03-19 22:47:19 +0700

    Group Project Phase 2 - Done: Bookmark Routes

--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const {
+    getAllBookmark,
+    updateBookmark,
+    deleteBookmark,
+    addBookmark,
+    getBookmarkById,
+    getByUserIdAndRecipeId,
+} = require("../controllers/BookmarkControllers");
 const bookmarkRouter = express.Router();
+
+bookmarkRouter.post("/", addBookmark);
+bookmarkRouter.get("/", getAllBookmark);
+bookmarkRouter.get("/:id", getBookmarkById);
+bookmarkRouter.put("/:id", updateBookmark);
+bookmarkRouter.post("/delete/:id", deleteBookmark);
+bookmarkRouter.get("/getbyid", getByUserIdAndRecipeId);
 
 module.exports = bookmarkRouter;
