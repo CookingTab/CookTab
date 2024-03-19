e26b85db71bdbf08053220306f5c91982cdee201
Zain <110021731+Zain031@users.noreply.github.com>
2024-03-19 22:17:22 +0000

    done,login

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -44,3 +44,36 @@ const addUser = async (req, res) => {
             .json({ message: "Error adding data", error: error.message });
     }
 };
+
+const loginWithCredentials = async (req, res) => {
+    try {
+        const body = req.body;
+        const { email, password } = body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        const user = userCredential.user;
+
+        // save refreshToken to cookie
+        let refreshToken = user?.stsTokenManager.refreshToken;
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 24 * 60 * 60 * 10000,
+        });
+
+        res.status(200).json({ message: "User logged in successfully", user });
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: "Error adding data", error: error.message });
+    }
+};
