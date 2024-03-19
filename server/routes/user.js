39043d8be99d86c9890a6f2c96f3c55967644435
NatasyaPutriS <natasyaputri.s@gmail.com>
2024-03-19 22:37:25 +0700

    Group Project Phase 2 - Add: currentUser Routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
     addUser,
     loginWithCredentials,
+    currentUser,
 } = require("../controllers/UserController");
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", addUser);
 userRouter.post("/signin", loginWithCredentials);
+userRouter.post("/profile", currentUser);
 
 module.exports = userRouter;
