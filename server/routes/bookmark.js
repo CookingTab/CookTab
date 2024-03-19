dcba53036b09188f63bbb17b809805fa72184208
NatasyaPutriS <natasyaputri.s@gmail.com>
2024-03-19 22:35:45 +0700

    Group Project Phase 2 - Setup Bookmark Routes

---
+++ server/routes/bookmark.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const bookmarkRouter = express.Router();
+
+module.exports = bookmarkRouter;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const bookmarkRouter = require("./bookmark");
+const userRouter = require("./user")
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).json({
         success: true,
-        message: "HALO GES",
+        message: "Hello World!",
     });
 });
+
+router.use("/bookmark", bookmarkRouter);
+router.use("/users", userRouter)
 
 module.exports = router;
