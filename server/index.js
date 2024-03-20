4d3491201c26299a8c2d97db4b243217caf94aee
Zain <110021731+Zain031@users.noreply.github.com>
2024-03-20 11:01:35 +0000

    feat: create user controller file and setup firebase auth

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.jsogit n());
 app.use(express.static("public"));
 app.use(router);
 
