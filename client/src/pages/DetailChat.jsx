5bb2296459500210a85ea751e38b1f3ae98437f6
MuhammadSyauqiAlBashir <bashirsyauqi@gmail.com>
2024-03-21 09:58:39 +0700

    final deploy

--- client/src/pages/DetailChat.jsx
+++ client/src/pages/DetailChat.jsx
@@ -6,14 +6,20 @@ import {
   query,
   serverTimestamp,
 } from "firebase/firestore";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { db } from "../libs/firebase";
 import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { BsSend } from "react-icons/bs";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const DetailChat = () => {
   const params = useParams();
+  const { theme, currentTheme, setCurrentTheme } = useContext(ThemeContext);
+  const text = theme[currentTheme].text;
+  const bgcolor = theme[currentTheme].bgcolor;
+  const bgcomps = theme[currentTheme].bgcomps;
+  const bgdetail = theme[currentTheme].bgdetail;
   const id = params.id;
 
   const [chatSnap, loading] = useDocument(doc(db, "chats", id));
@@ -62,57 +68,64 @@ const DetailChat = () => {
       </div>
     );
   }
-  
+
   return (
-    <div className="max-w-2xl mx-auto p-6 flex flex-col h-[90dvh]">
-      <div className="w-full p-3 border-b flex gap-2 items-center">
-        <a href="/chat" className="p-2 rounded-sm border">
-          Back
-        </a>
-        <h1 className="text-lg font-bold">{chat?.name}</h1>
-      </div>
-      <div className="flex-1 w-full h-full overflow-y-auto">
-        {loading2 ? (
-          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-slate-800"></div>
-        ) : (
-          <div className="w-full overflow-y-auto h-full">
-            {message?.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`w-full flex my-2 ${
-                    item?.sender === "admin" ? "justify-end" : "justify-start"
-                  }`}
-                >
+    <div className={`${bgcolor} h-screen fixed w-screen`}>
+      <div className="max-w-2xl mx-auto p-6 flex flex-col h-[90dvh]">
+        <div
+          className={`${bgcomps} w-full p-3 border-b flex gap-2 items-center`}
+        >
+          <Link to="/chat" className={`${text} p-2 rounded-sm border`}>
+            Back
+          </Link>
+          <h1 className={`${text} text-lg font-bold`}>{chat?.name}</h1>
+        </div>
+        <div className="flex-1 w-full h-full overflow-y-auto">
+          {loading2 ? (
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-slate-800"></div>
+          ) : (
+            <div className={`${bgcomps} w-full overflow-y-auto h-full`}>
+              {message?.map((item, index) => {
+                return (
                   <div
-                    className={`w-[70%] p-2 rounded-sm ${
-                      item?.sender === "admin"
-                        ? "bg-blue-500 text-white"
-                        : "bg-slate-200"
+                    key={index}
+                    className={`w-full flex my-2 ${
+                      item?.sender === "admin" ? "justify-end" : "justify-start"
                     }`}
                   >
-                    <p>{item?.message}</p>
+                    <div
+                      className={`w-[70%] p-2 rounded-sm ${
+                        item?.sender === "admin"
+                          ? "bg-blue-500 text-white"
+                          : "bg-slate-200"
+                      }`}
+                    >
+                      <p>{item?.message}</p>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-            <div ref={bottomOfChat}></div>
-          </div>
-        )}
+                );
+              })}
+              <div ref={bottomOfChat}></div>
+            </div>
+          )}
+        </div>
+        <form
+          onSubmit={sendMessage}
+          className="p-3 border rounded-sm flex gap-2"
+        >
+          <input
+            type="text"
+            required
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            className="flex-1 p-2 border-b focus:outline-blue-500"
+            placeholder="Enter message"
+          />
+          <button className="p-2 bg-blue-500 text-white rounded-sm">
+            <BsSend />
+          </button>
+        </form>
       </div>
-      <form onSubmit={sendMessage} className="p-3 border rounded-sm flex gap-2">
-        <input
-          type="text"
-          required
-          value={msg}
-          onChange={(e) => setMsg(e.target.value)}
-          className="flex-1 p-2 border-b focus:outline-blue-500"
-          placeholder="Enter message"
-        />
-        <button className="p-2 bg-blue-500 text-white rounded-sm">
-          <BsSend />
-        </button>
-      </form>
     </div>
   );
 };
